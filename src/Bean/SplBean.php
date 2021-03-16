6c254376ae0eb4bb6bb1b4b480284ec016fbb3de
nashgao <nashgao1992@gmail.com>
2021-03-16 11:35:45 +1000

    first commit

---
+++ src/Bean/SplBean.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nashgao\Utils\Bean;
+
+use EasySwoole\Spl\SplBean as Bean;
+
+class SplBean extends Bean
+{
+    /**
+     * @param null $filter
+     */
+    public function toArray(array $columns = null, $filter = null): array
+    {
+        if (! isset($filter)) { // if filter is not specified, then use not null filter
+            $filter = Bean::FILTER_NOT_NULL;
+        }
+
+        $array = parent::toArray($columns, $filter);
+        array_walk_recursive($array, function (&$item, $key) use ($filter) {
+            if (! is_scalar($item) and $item instanceof Bean) {
+                $item = $item->toArray(null, $filter);
+            }
+        });
+        return $array;
+    }
+
+    /**
+     * @param null $filter
+     */
+    public function toArrayWithOneDimension(array $columns = null, $filter = null): array
+    {
+        return parent::toArray($columns, $filter);
+    }
+}
