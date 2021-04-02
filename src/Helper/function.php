49576a81f17babd03ed1bd9d2d9df2bb43f2e6d1
nashgao <nashgao1992@gmail.com>
2021-04-02 13:56:01 +1000

    update function

--- src/Helper/function.php
+++ src/Helper/function.php
@@ -2,16 +2,23 @@
 
 declare(strict_types=1);
 
+use Hyperf\Contract\ConfigInterface;
 use Hyperf\Utils\ApplicationContext;
 use Nashgao\Utils\Bean\SplBean;
 use Psr\Container\ContainerInterface;
 use Swoole\Server;
-use Hyperf\Contract\ConfigInterface;
 
 if (! function_exists('di')) {
     function di(): ContainerInterface
     {
         return ApplicationContext::getContainer();
+    }
+}
+
+if (! function_exists('dispatch')) {
+    function dispatch(object $event): object
+    {
+        return di()->get(EventDispatcherInterface::class)->dispatch($event);
     }
 }
 
@@ -50,10 +57,9 @@ if (! function_exists('getWorkerId')) {
             $server = getServer();
             if (! $server->taskworker) {
                 return $server->worker_id;
-            } else {
-                $workerNum = di()->get(ConfigInterface::class)->get("server.settings.worker_num");
-                return $workerNum + $server->worker_id;
             }
+            $workerNum = di()->get(ConfigInterface::class)->get('server.settings.worker_num');
+            return $workerNum + $server->worker_id;
         } catch (\Throwable $throwable) {
             return 0;
         }
